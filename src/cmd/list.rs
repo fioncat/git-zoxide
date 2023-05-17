56c703ec74e3ddda507202b7660c735a3d281cfc
wenqian <lazycat7706@gmail.com>
2023-05-17 14:45:21 +0800

    fix(github): use issue query to get pr (#15)

--- src/cmd/list.rs
+++ src/cmd/list.rs
@@ -12,47 +12,43 @@ impl Run for List {
     fn run(&self) -> Result<()> {
         let cfg = Config::parse()?;
 
-        if self.keyword {
-            let now = util::current_time()?;
-            let db = Database::open()?;
-            let keywords = Keywords::open(now)?;
-            let mut name_set = HashSet::with_capacity(db.repos.len() + keywords.data.len());
-            for repo in &db.repos {
-                let (_, name) = util::split_name(&repo.name);
-                if let Some(_) = name_set.get(&name) {
-                    continue;
-                }
-                println!("{}", name);
-                name_set.insert(name);
-            }
-
-            let keywords = keywords.list();
-            for keyword in keywords {
-                if let Some(_) = name_set.get(keyword) {
-                    continue;
-                }
-                name_set.insert(keyword.to_string());
-                println!("{}", keyword);
-            }
-
-            let mut keys: Vec<_> = cfg
-                .keyword_map
-                .iter()
-                .map(|(key, _)| key.to_string())
-                .collect();
-            keys.sort_by(|s1, s2| s1.cmp(&s2));
-            for key in keys {
-                if let None = name_set.get(&key) {
-                    println!("{}", key);
-                }
-            }
-
-            return Ok(());
-        }
-
         if self.args.is_empty() {
-            for remote in &cfg.remotes {
-                println!("{}", remote.name);
+            let keyword = self.keyword;
+            let remote = if !self.remote && !self.keyword {
+                true
+            } else {
+                self.remote
+            };
+            if keyword {
+                let now = util::current_time()?;
+                let keywords = Keywords::open(now)?;
+                let mut name_set = HashSet::with_capacity(keywords.data.len());
+
+                let keywords = keywords.list();
+                for keyword in keywords {
+                    if let Some(_) = name_set.get(keyword) {
+                        continue;
+                    }
+                    name_set.insert(keyword.to_string());
+                    println!("{}", keyword);
+                }
+
+                let mut keys: Vec<_> = cfg
+                    .keyword_map
+                    .iter()
+                    .map(|(key, _)| key.to_string())
+                    .collect();
+                keys.sort_by(|s1, s2| s1.cmp(&s2));
+                for key in keys {
+                    if let None = name_set.get(&key) {
+                        println!("{}", key);
+                    }
+                }
+            }
+            if remote {
+                for remote in &cfg.remotes {
+                    println!("{}", remote.name);
+                }
             }
             return Ok(());
         }
